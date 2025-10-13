fd94b00c651a419595419b5e7fe1060f5cab86b2
Vitor Campos <vitor.campos@spassu.com.br>
2025-10-13 11:02:30 -0300

    chore: Static config

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
