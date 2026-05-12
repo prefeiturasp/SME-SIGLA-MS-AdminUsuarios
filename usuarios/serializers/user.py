ffe4b975de1bb880a3ec24124a5e3b8ab45b8829
dennysdf <dennys_ps_@live.com>
2026-05-12 09:37:09 -0300

    feat(148085):criação de novo usuário no SIGLA

--- usuarios/serializers/user.py
+++ usuarios/serializers/user.py
@@ -1,15 +1,13 @@
 from rest_framework import serializers
 
 
-class CreateUserSerializer(serializers.Serializer):
-    usuario = serializers.CharField()
-    senha = serializers.CharField(write_only=True)
-    email = serializers.EmailField()
-    
-    def validate(self, data):
-        # Converte usuario -> username e senha -> password após validação
-        data['username'] = data.pop('usuario')
-        data['password'] = data.pop('senha')
-        return data
+class BuscarUsuarioEolSerializer(serializers.Serializer):
+    rf = serializers.CharField()
 
+
+class CreateUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    nome = serializers.CharField()
+    email = serializers.EmailField()
+    senha = serializers.CharField(write_only=True)
 
